01b3aabffc624bda83475287c1d77a228b5c4c2c
Luiz Felipe <luiz.leite@linkedrh.com.br>
2024-06-17 16:09:28 -0300

    feat: created login page

--- apps/dashboard/src/app/app.config.ts
+++ apps/dashboard/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideNgxMask({ /* opções de cfg */ })
   ],
 };
--- apps/dashboard/src/app/login/login.component.ts
+++ apps/dashboard/src/app/login/login.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { NgxMaskDirective } from 'ngx-mask';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Observable, Subject, combineLatest, takeUntil } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxMaskDirective, ReactiveFormsModule, FormsModule],
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
+  public isActive = false;
+
+  public isPasswordEqual = false
+
+  public form!: FormGroup;
+
+  private unsubscribe = new Subject();
+
+  public cpf = '';
+
+  public password = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private route: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.getPasswordValue();
+  }
+
+  ngOnDestroy(): void {
+      this.unsubscribe.next(null);
+      this.unsubscribe.complete();
+  }
+
+  public onLogin(): void {
+    console.log(this.cpf, this.password);
+
+  }
+
+  public onSignUp(): void {
+    console.log('asdsa');
+  }
+
+  public goToHome(): void {
+    this.route.navigate(['home']);
+  }
+
+  public getPasswordValue(): void {
+    combineLatest([
+      this.form.get('password')?.valueChanges as Observable<string>,
+      this.form.get('confirmPassword')?.valueChanges as Observable<string>
+    ]).pipe(takeUntil(this.unsubscribe)).subscribe(
+      ([password, confirmPassword]) => {
+        this.isPasswordEqual = password === confirmPassword
+        console.log(this.isPasswordEqual);
+
+      }
+    )
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      cpf: ['', Validators.required],
+      cellphone: [''],
+      birthday: ['', Validators.required],
+    })
+  }
 }
