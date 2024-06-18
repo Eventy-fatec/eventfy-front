28c02398c1e04c5c7c79a1ad9ca0c093d344cc66
Cesar Solano <cesarsolano0479@gmail.com>
2024-06-17 22:35:13 -0300

    fix: build issues

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GoogleMapsModule } from '@angular/google-maps';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, GoogleMapsModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -20,20 +19,9 @@ export class HomeComponent implements OnInit{
   ){}
 
   ngOnInit(): void {
-    this.loadMap()
   }
 
-  loadMap() {
-    const mapProp = {
-      center: new google.maps.LatLng(40.712775, -74.005973),
-      zoom: 18,
-    };
-    
-    this.options = new google.maps.Map(
-      document.getElementById('googleMap') as HTMLElement,
-      mapProp
-    );
-  }
+  
 
   goToLogin() {
     this.route.navigate(['login'])
